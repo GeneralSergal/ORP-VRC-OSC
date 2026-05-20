afb37d03f177f7a2f07328d4c99cec979ebfc89e
Laurentius Maximus <22939657+GeneralSergal@users.noreply.github.com>
2026-05-20 23:38:36 +0200

    Delete modules directory

--- modules/safe_mode.py
+++
@@ -1,9 +0,0 @@
-from state import state
-
-def safe_mode_handler(address, *args):
-    if args:
-        state["safe_mode"] = bool(args[0])
-
-        print(
-            f"!! SAFE MODE: {state['safe_mode']} !!"
-        )
