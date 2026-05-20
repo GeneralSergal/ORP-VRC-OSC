c3502f954ba6440ab6c77cc5d0d86bbebfc8c8cb
Laurentius Maximus <22939657+GeneralSergal@users.noreply.github.com>
2026-05-20 17:03:45 +0200

    Add files via upload

---
+++ launch_osc.bat
@@ -0,0 +1,13 @@
+@echo off
+title ORP VRC OSC Middleware
+
+cd /d "%~dp0"
+
+echo ==========================================
+echo ORP VRC OSC Middleware
+echo ==========================================
+echo.
+
+python osc_vrc_bridge.py
+
+pause
